46d146fc1ff592cfd41f64191be3e0443747cbe5
spidergroup <spidergroup@users.noreply.github.com>
2016-05-04 23:39:41 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@ add_filter('nav_menu_css_class' , 'set_active_nav_class' , 10 , 2);
 function set_active_nav_class ($classes, $item) {
 	 
     if (in_array('current-menu-item', $classes) ){
-        $classes[] = 'active ';
+        $classes[] = 'activemenu ';
     }
     return $classes;
 }
