fd6a3659aa8e4cc42c060d9bd11087bf6e71a2f4
spider group <sambo.sok@gmail.com>
2016-06-15 01:04:12 -0500

    Use page template by category name

--- page-template-home.php
+++ page-template-home.php
@@ -1,66 +1,39 @@
 <?php
 
 /*
-Template Name: wHome with 3 Columns
+Template Name: Page Template - WooCommerce Category
 */
+?>
 
-get_header();
+<?php
+ 
+    get_header();
 
-$args = array(
-	'post_type' => 'post'
-);
-$query = new WP_Query( $args );
+    $args = array(
+        'post_type' => 'post',
+        'category_name' => 'woocommerce'
+       
+    );
+    $query = new WP_Query( $args );
+?>
 
-if ($query->have_posts()) : ?>
 
-
-    <!-- Begin grid with 3 columns -->
-    <div class="row">
-           
-        <?php while ($query->have_posts()) : $query->the_post(); ?>
-        
-            <div class="col-sm-6 col-md-4">
-                    
-                    <?php 
-                        $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); 
-                         
-                    ?>
-                    
-                    <div class="thumbnail threecolumns">
-                        <?php if ( ! empty( $large_image_url[0] ) ) : ?>
-                            <a href="<?php the_permalink(); ?>">
-                                <img src="<?php echo $large_image_url[0]; ?>" alt="..." >
-                            </a>
-                            
-                        <?php endif; ?>
-
-                      <div class="caption text-center">
-                            <h4>
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </h4>
-                            <!--<p><?php the_content(); ?></p>-->
-                      </div>
-                    </div>
-            </div>
-                    
-        
-        
-        <?php endwhile;
-        
-        else :
-            echo '<p>No content found</p>';
-        
-        endif;
-        
-        wp_reset_postdata();
-	?>
+<?php if ($query->have_posts()) : ?>
     
-    </div><!-- End grid with 3 columns -->
-              
-              
-    <?php
-get_footer();
+    <?php while ($query->have_posts()) : $query->the_post(); ?>
+        <div class="row">
+            <?php get_template_part('content'); ?>
+        </div>
+    <?php endwhile; ?>
+
+<?php else : ?>
+    <p>No content found</p>
+<?php endif; ?>    
+
+
+<?php
+    wp_reset_postdata();
+	 
+    get_footer();
 
 ?>
